f8fdb49099254a2712e0558120cc1a242b04adc9
Aman Chauhan <aman.chauhan@tothenew.com>
2025-08-23 14:41:06 +0530

    change api server

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://lms-website-h0l1.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
