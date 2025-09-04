e06541ac5f62485bb2d2596b720b6ef253fccece
Aman Chauhan <aman.chauhan@tothenew.com>
2025-09-04 13:33:52 +0530

    vercel config changes

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_REACT_APP_BACKEND_BASE_URL,
   // baseURL: "https://lms-website-h0l1.onrender.com",
 });
 
