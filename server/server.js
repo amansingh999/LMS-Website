9792ee3958fd77c780d5e708d3607221d2a2e28d
Aman Chauhan <aman.chauhan@tothenew.com>
2025-09-04 23:22:23 +0530

    update cors origin setting

--- server/server.js
+++ server/server.js
@@ -16,13 +16,14 @@ const MONGO_URI = process.env.MONGO_URI;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: '*',
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 app.use(express.json());
+app.options("*", cors());
 
 //database connection
 mongoose
